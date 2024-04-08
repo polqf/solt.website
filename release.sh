2f538856a2186ec46e8247f72bd0a70ce477a425
Pol Quintana <pol.quintana1@gmail.com>
2024-04-08 10:25:51 +0200

    HTML only website

--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-systemctl stop solt
-systemctl stop nginx
-swift build -c release && cp `swift build -c release --show-bin-path`/Run ./executable/solt
-systemctl start solt
-systemctl start nginx
